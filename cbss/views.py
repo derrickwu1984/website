b0601fc3813fc4c3e6d1f660f4523733cc397824
derrickwu1984 <derrickwu@sina.com>
2019-02-22 16:20:19 +0800

    增加网站模板

--- cbss/views.py
+++ cbss/views.py
@@ -3,7 +3,8 @@ from .models import mobile_userinfo_jinan
 # Create your views here.
 import  logging
 def getList(request):
-    infoList=mobile_userinfo_jinan.objects.all()
-    for info in infoList:
-        logging.warning(info.paytype)
-    return render(request, 'userInfoList.html')
+    # infoList=mobile_userinfo_jinan.objects.all()
+    # for info in infoList:
+    #     logging.warning(info.paytype)
+    # return render(request, 'userInfoList.html')
+    return render(request, 'index.html')
